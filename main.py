ea986544e60ce74f0ca9af7f4814bc07cc3af4b6
abduqodirovelbek66-cmd <abduqodirovelbek66@gmail.com>
2026-06-08 19:02:59 +0500

    Change bot token in main.py
    Updated the bot token for authentication.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import telebot
 import time
 
 # ⚠️ Bot tokeningizni BotFather'dan olib, shu yerga yozing!
-TOKEN = "8625467620:AAG_1hFvi5Q5UcCR2jSE2msWD25BGrLkaL4"
+TOKEN = "8359740634:AAFyC6qqIMpmnbS5mHxg1gSB5JG9XducRi4"
 bot = telebot.TeleBot(TOKEN)
 
 # ⚠️ Sizning telegram ID raqamingiz (Faqat siz video yuklay olasiz)
