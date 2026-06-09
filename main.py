2250dbc097eebd681a32f82f25ef90d4602008a1
abduqodirovelbek66-cmd <abduqodirovelbek66@gmail.com>
2026-06-09 10:50:46 +0500

    Update movie details and add channel button

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 TOKEN = "8650658473:AAEZ_A0VjLfxeRVELet0Q87ztZkOmr4Acfg"
-CHANNEL = "https://t.me/clipzXorg"
+CHANNEL = "@clipzXorg"
 
 bot = telebot.TeleBot(TOKEN)
 
 # Kino bazasi
 movies = {
-    "1001": "🎬 Kino nomi: Ovchi Itlar 2 -FASL Barcha qisim\n📥 Link:https://t.me/c/3511706384/9",
+    "1001": "🎬 Kino nomi: Ovchi itlar 2-FASL Barcha qisimlar\n📥 Link: https://t.me/c/3511706384/9",
     "1002": "🎬 Kino nomi: Interstellar\n📥 Link: https://example.com/movie2",
     "1003": "🎬 Kino nomi: Avengers\n📥 Link: https://example.com/movie3"
 }
+
+def channel_button():
+    markup = InlineKeyboardMarkup()
+    btn = InlineKeyboardButton(
+        "📢 KANAL",
+        url="https://t.me/clipzXorg"
+    )
+    markup.add(btn)
+    return markup
 
 def check_sub(user_id):
     try:
@@ -19,6 +29,14 @@ def check_sub(user_id):
     except:
         return False
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(
+        message.chat.id,
+        "👋 Botimizga xush kelibsiz!\n\n📢 Kanalga obuna bo'ling 😊",
+        reply_markup=channel_button()
+    )
+
 @bot.message_handler(func=lambda message: True)
 def get_movie(message):
     user_id = message.from_user.id
@@ -26,7 +44,8 @@ def get_movie(message):
     if not check_sub(user_id):
         bot.reply_to(
             message,
-            "Kanalga obuna bo'ling 😊"
+            "📢 Kanalga obuna bo'ling 😊",
+            reply_markup=channel_button()
         )
         return
 
